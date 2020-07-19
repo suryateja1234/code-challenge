5a4dc2cea02455dc4f4dfac07d029b102428a73f
SuryaTeja <teja.paduri@gmail.com>
2020-07-19 21:57:45 +0530

    added test cases

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -47,7 +47,7 @@ class CompaniesControllerTest < ApplicationSystemTestCase
       fill_in("company_name", with: "New Test Company")
       fill_in("company_zip_code", with: "28173")
       fill_in("company_phone", with: "5553335555")
-      fill_in("company_email", with: "new_test_company@test.com")
+      fill_in("company_email", with: "new_test_company@getmainstreet.com")
       click_button "Create Company"
     end
 
@@ -56,6 +56,42 @@ class CompaniesControllerTest < ApplicationSystemTestCase
     last_company = Company.last
     assert_equal "New Test Company", last_company.name
     assert_equal "28173", last_company.zip_code
+  end
+
+  test "Delete" do
+    visit company_path(@company)
+    assert_difference('Company.count', -1) do
+      click_link "Delete"
+    end
+  end
+
+  test "Validating Email" do
+    visit new_company_path
+
+    within("form#new_company") do
+      fill_in("company_name", with: "New Test Company")
+      fill_in("company_zip_code", with: "28173")
+      fill_in("company_phone", with: "5553335555")
+      fill_in("company_email", with: "new_test_company@getmainstreet.com")
+      assert_no_difference('Company.count') do
+        click_button 'Create Company'
+      end
+    end
+  end
+
+  test "Fetching city and state from zipcode" do
+
+    within("form#edit_company_#{@company.id}") do
+      fill_in("company_zip_code", with: "28173")
+      click_button "Update Company"
+    end
+
+    assert_text "Changes Saved"
+
+    @company.reload
+    assert_equal "28173", @company.zip_code
+    assert_equal "North Carolina", @company.city
+    assert_equal "NC", @company.state
   end
 
 end
